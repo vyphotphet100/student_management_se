36d07ad1ec08e796380da71d8ff644365d68676d
vyphotphet100 <69227424+vyphotphet100@users.noreply.github.com>
2021-06-18 02:13:37 +0700

    18/6

---
+++ Student/notification_click/main.js
@@ -0,0 +1,23 @@
+(function() {
+    'use strict'
+
+    feather.replace()
+})();
+
+function loadFullNotification() {
+    var id = common.getParameterByName('id');
+    var notificationDto = NotificationRequest.findOne(id);
+
+    if (notificationDto.httpStatus != 'OK') {
+        alert(notificationDto.message);
+        return;
+    }
+
+    document.getElementById('title').value = notificationDto.title;
+    document.getElementById('content').value = notificationDto.content;
+}
+
+function main() {
+    loadFullNotification();
+}
+main();
